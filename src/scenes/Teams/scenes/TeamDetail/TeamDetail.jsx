0653f221256a5517a9c0435af4049341f96bcb86
Joris Lenclume <contact@jorislenclume.com>
2020-02-27 09:01:52 +0100

    Add citation of manager

--- src/scenes/Teams/scenes/TeamDetail/TeamDetail.jsx
+++ src/scenes/Teams/scenes/TeamDetail/TeamDetail.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { Grid } from '@material-ui/core'
 import { SubHeader } from './components'
-import { Collaborator, EmptyState, GridLink, MainLayoutComponent } from '../../../../components'
+import {Collaborator, EmptyState, GridLink, InfoText, MainLayoutComponent} from '../../../../components'
 import * as teamDetailActions from '../../../../services/Teams/TeamDetail/actions'
 
 class TeamDetail extends MainLayoutComponent {
@@ -21,24 +21,29 @@ class TeamDetail extends MainLayoutComponent {
     }
 
     renderData() {
-        const { team } = this.props.teamDetail
+        const { team } = this.props.teamDetail;
 
         return (
-            <Grid container spacing={2}>
+            <Grid container spacing={2} style={{marginTop: 8}}>
+                { team.manager && team.manager.citation && <Grid xs={12} style={{marginBottom: 8}}>
+                    <InfoText align='center'>
+                        « {team.manager.citation} »
+                    </InfoText>
+                </Grid> }
                 { team.collaborators.map(collaborator => {
                     return (
                         <GridLink key={collaborator.id} item xs={12} sm={6} md={4} component={Link} to={`/teams/${this.props.match.params.id}/collaborators/${collaborator.id}/detail`}>
                             <Collaborator collaborator={collaborator} />
                         </GridLink>
                     )
-                }) }        
+                }) }
             </Grid>
         )
     }
 
     render() {
         const { team, loading } = this.props.teamDetail;
-        
+
         return (
             <div>
                 { !loading && team && this.renderData() }
