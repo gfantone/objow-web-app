e95a348915f66bcab186f1fefe5fdff1403f4d8d
Joris Lenclume <contact@jorislenclume.com>
2020-06-16 12:15:07 +0200

    Add button to go to reward creation form

--- src/scenes/Rewards/scenes/TeamRewardOrderTracking/TeamRewardOrderTracking.jsx
+++ src/scenes/Rewards/scenes/TeamRewardOrderTracking/TeamRewardOrderTracking.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import {ValidatedTeamRewardOrderList, WaitingTeamRewardOrderList} from './components'
 import {TrackingSubHeader} from '../../components'
-import {MainLayoutComponent} from '../../../../components'
+import {IconButton as AppBarIconButton, MainLayoutComponent} from '../../../../components'
 import * as Resources from '../../../../Resources'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faPlus} from "@fortawesome/free-solid-svg-icons";
 
 class TeamRewardOrderTracking extends MainLayoutComponent {
     state = {page: 0}
+
+    handleAdd() {
+        this.props.history.push('/rewards/creation')
+    }
 
     handlePageChange(page) {
         this.setState({
@@ -17,6 +23,7 @@ class TeamRewardOrderTracking extends MainLayoutComponent {
     componentDidMount() {
         this.props.handleTitle(Resources.REWARD_TITLE)
         this.props.handleSubHeader(<TrackingSubHeader onChange={this.handlePageChange.bind(this)} />)
+        this.props.handleButtons(<AppBarIconButton size='small' onClick={this.handleAdd.bind(this)}><FontAwesomeIcon icon={faPlus} /></AppBarIconButton>);
         this.props.activateReturn()
     }
 
