1197ed177387d00b03d9c9156ca3f8ff980612ce
Damien <d.corticchiato@firetiger.fr>
2021-07-02 18:12:41 +0200

    fix lowercase default text

--- src/components/Common/components/Texts/components/DefaultText/DefaultText.jsx
+++ src/components/Common/components/Texts/components/DefaultText/DefaultText.jsx
@@ -3,20 +3,20 @@ import {Typography} from '@material-ui/core'
 import {withStyles} from '@material-ui/core/styles'
 
 const styles = {
-    rootUppercase: {
+    root: {
         fontSize: 13,
         textTransform: 'uppercase',
         color: '#555555',
     },
-    rootLowercase: {
+    lowercaseRoot: {
       textTransform: 'none',
       fontSize: 16,
       color: '#555555',
     }
 }
 
-const DefaultText = (props) => {
-  return <Typography className={props.lowercase ? props.classes.rootLowercase : props.classes.rootUppercase} {...props} />
-}
+const DefaultText = (props) => (
+  <Typography {...props} classes={{ root: props.lowercase ? props.classes.lowercaseRoot : props.classes.root }} />
+)
 
 export default withStyles(styles)(DefaultText)
