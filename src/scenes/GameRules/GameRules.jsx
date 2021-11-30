e79d3e29b95a8e5c10a9b7467aeac28583371064
Damien Corticchiato <d.corticchiato@firetiger.fr>
2021-11-30 17:05:50 +0100

    fix help page

--- src/scenes/GameRules/GameRules.jsx
+++ src/scenes/GameRules/GameRules.jsx
@@ -11,56 +11,6 @@ import {faEdit} from "@fortawesome/free-solid-svg-icons";
 import * as configUpdateActions from '../../services/Configs/ConfigUpdate/actions'
 
 
-function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window;
-  return {
-    width,
-    height
-  };
-}
-
-const RulesIframe = ({...props}) => {
-  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-
-  useEffect(() => {
-    function handleResize() {
-      setWindowDimensions(getWindowDimensions());
-    }
-
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
-  const isLg = isWidthUp('lg', props.width)
-  const isMd = isWidthUp('md', props.width)
-  const isSm = isWidthUp('sm', props.width)
-  const isXs = isWidthUp('xs', props.width)
-  let marginLeft = -20
-  let width = '100vw'
-  const marginTop = -20
-
-  if(isLg) {
-    marginLeft =  `-${((windowDimensions.width - 304 - 915) / 2)}px`
-    width = `${windowDimensions.width - 304}px`
-  } else if (isMd) {
-    marginLeft =  `-${((windowDimensions.width - 915) / 2)}px`
-    width = '100vw'
-
-  } else if (isSm) {
-    marginLeft = -20
-    width = '100vw'
-
-  } else if (isXs) {
-    marginLeft = 0
-    width = '100vw'
-
-  }
-  return (
-    <iframe src="https://firetiger.fr/help" style={{width, height: 'calc(100vh + 20px)', marginTop, marginLeft}} frameBorder="0"/>
-  )
-}
-
-
 class GameRules extends MainLayoutComponent {
     state = {edition: false, rules: null}
 
@@ -116,23 +66,18 @@ class GameRules extends MainLayoutComponent {
           return <Redirect to={'/'} />
         }
 
-        // <Grid container spacing={2} justify='space-between'>
-        // <Grid item xs={12} style={{ marginTop: '20px' }}>
-        //   <RichText initial={rules} readOnly={!this.state.edition} onChange={this.handleRulesChange.bind(this)} />
-        // </Grid>
-        // {this.state.edition && <Grid item>
-        //   <Button color='secondary' onClick={() => this.props.history.go(0)}>{Resources.GAME_RULES_CANCEL_BUTTON}</Button>
-        // </Grid>}
-        // {this.state.edition && <Grid item>
-        //   <ProgressButton loading={loading} text={Resources.GAME_RULES_SUBMIT_BUTTON} onClick={this.handleSaveClick.bind(this)} />
-        // </Grid>}
-        // </Grid>
-
-        const Rules = withWidth()(RulesIframe)
         return (
-              <div>
-                <Rules />
-              </div>
+          <Grid container spacing={2} justify='space-between'>
+            <Grid item xs={12} style={{ marginTop: '20px' }}>
+              <RichText initial={rules} readOnly={!this.state.edition} onChange={this.handleRulesChange.bind(this)} />
+            </Grid>
+            {this.state.edition && <Grid item>
+              <Button color='secondary' onClick={() => this.props.history.go(0)}>{Resources.GAME_RULES_CANCEL_BUTTON}</Button>
+            </Grid>}
+            {this.state.edition && <Grid item>
+              <ProgressButton loading={loading} text={Resources.GAME_RULES_SUBMIT_BUTTON} onClick={this.handleSaveClick.bind(this)} />
+            </Grid>}
+          </Grid>
         )
     }
 
@@ -165,4 +110,4 @@ const mapDispatchToProps = (dispatch) => ({
     configUpdateActions: bindActionCreators(configUpdateActions, dispatch)
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(withWidth()(GameRules))
+export default connect(mapStateToProps, mapDispatchToProps)(GameRules)
