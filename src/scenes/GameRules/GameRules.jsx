2d4d63c0c39cf0e08a3c511542bcf03df910648a
Damien Corticchiato <d.corticchiato@firetiger.fr>
2021-11-30 12:26:59 +0100

    add game rules in iframe

--- src/scenes/GameRules/GameRules.jsx
+++ src/scenes/GameRules/GameRules.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
-import {Grid} from '@material-ui/core'
+import {Grid, isWidthUp, withWidth} from '@material-ui/core'
 import {Redirect} from 'react-router-dom'
 import {Button, IconButton as AppBarIconButton, Loader, MainLayoutComponent, ProgressButton, RichText} from '../../components'
 import * as Resources from '../../Resources'
@@ -9,6 +9,57 @@ import * as configListActions from '../../services/Configs/ConfigList/actions'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faEdit} from "@fortawesome/free-solid-svg-icons";
 import * as configUpdateActions from '../../services/Configs/ConfigUpdate/actions'
+
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+}
+
+const RulesIframe = ({...props}) => {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const isLg = isWidthUp('lg', props.width)
+  const isMd = isWidthUp('md', props.width)
+  const isSm = isWidthUp('sm', props.width)
+  const isXs = isWidthUp('xs', props.width)
+  let marginLeft = -20
+  let width = '100vw'
+  const marginTop = -20
+
+  if(isLg) {
+    marginLeft =  `-${((windowDimensions.width - 304 - 915) / 2)}px`
+    width = `${windowDimensions.width - 304}px`
+  } else if (isMd) {
+    marginLeft =  `-${((windowDimensions.width - 915) / 2)}px`
+    width = '100vw'
+
+  } else if (isSm) {
+    marginLeft = -20
+    width = '100vw'
+
+  } else if (isXs) {
+    marginLeft = 0
+    width = '100vw'
+
+  }
+  return (
+    <iframe src="https://firetiger.fr/help" style={{width, height: 'calc(100vh + 20px)', marginTop, marginLeft}} frameBorder="0"/>
+  )
+}
+
 
 class GameRules extends MainLayoutComponent {
     state = {edition: false, rules: null}
@@ -65,18 +116,23 @@ class GameRules extends MainLayoutComponent {
           return <Redirect to={'/'} />
         }
 
+        // <Grid container spacing={2} justify='space-between'>
+        // <Grid item xs={12} style={{ marginTop: '20px' }}>
+        //   <RichText initial={rules} readOnly={!this.state.edition} onChange={this.handleRulesChange.bind(this)} />
+        // </Grid>
+        // {this.state.edition && <Grid item>
+        //   <Button color='secondary' onClick={() => this.props.history.go(0)}>{Resources.GAME_RULES_CANCEL_BUTTON}</Button>
+        // </Grid>}
+        // {this.state.edition && <Grid item>
+        //   <ProgressButton loading={loading} text={Resources.GAME_RULES_SUBMIT_BUTTON} onClick={this.handleSaveClick.bind(this)} />
+        // </Grid>}
+        // </Grid>
+
+        const Rules = withWidth()(RulesIframe)
         return (
-            <Grid container spacing={2} justify='space-between'>
-                <Grid item xs={12} style={{ marginTop: '20px' }}>
-                    <RichText initial={rules} readOnly={!this.state.edition} onChange={this.handleRulesChange.bind(this)} />
-                </Grid>
-                {this.state.edition && <Grid item>
-                    <Button color='secondary' onClick={() => this.props.history.go(0)}>{Resources.GAME_RULES_CANCEL_BUTTON}</Button>
-                </Grid>}
-                {this.state.edition && <Grid item>
-                    <ProgressButton loading={loading} text={Resources.GAME_RULES_SUBMIT_BUTTON} onClick={this.handleSaveClick.bind(this)} />
-                </Grid>}
-            </Grid>
+              <div>
+                <Rules />
+              </div>
         )
     }
 
@@ -109,4 +165,4 @@ const mapDispatchToProps = (dispatch) => ({
     configUpdateActions: bindActionCreators(configUpdateActions, dispatch)
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(GameRules)
+export default connect(mapStateToProps, mapDispatchToProps)(withWidth()(GameRules))
