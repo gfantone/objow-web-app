af08f0167d4d46526b2454585553f9631d28588c
gfantone <gilles.fantone@gmail.com>
2024-06-28 22:57:11 +0200

    Update config.jsx

--- src/auth/config.jsx
+++ src/auth/config.jsx
@@ -1,8 +1,8 @@
 export const config = {
-    authority: 'http://localhost:8080/realms/edenred', // Remplacez par l'URL de votre serveur Keycloak et de votre realm
+    authority: process.env.REACT_APP_KEYCLOAK_URL, // Utilise l'URL de votre serveur Keycloak et de votre realm depuis l'environnement
     automaticSilentRenew: true,
-    client_id: 'edenred', // Remplacez par l'ID de votre client Keycloak
-    redirect_uri: 'http://localhost:3000/callback', // Remplacez par l'URL de redirection après l'authentification
+    client_id: process.env.REACT_APP_CLIENT_ID, // Utilise l'ID de votre client Keycloak depuis l'environnement
+    redirect_uri: `${process.env.REACT_APP_REDIRECT_URI}`, // Utilise l'URL de redirection depuis l'environnement
     response_type: 'code',
     scope: 'openid profile email', // Les scopes dont vous avez besoin
     loadUserInfo: true, // Charger les informations utilisateur
